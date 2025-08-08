9cdcf728afee4fb6df59344ef187f377be77f313
Ryan Lastufka <ryan.lastufka@gmail.com>
2025-08-07 23:34:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ command, mode }) => {
 
   return {
     plugins: [preact()],
-    base: isProduction ? '/Prosite/' : '/',
+    // base: isProduction ? '/Prosite/' : '/',
+    base: '/Prosite/',
   };
 });
