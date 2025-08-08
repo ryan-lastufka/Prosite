ae046d1136a4b7e4247caa5ec25cf29702f4e53c
Ryan Lastufka <ryan.lastufka@gmail.com>
2025-08-07 23:14:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    // Configuration for development server
-    return {
-      plugins: [preact()],
-      base: '/', 
-    };
-  } else {
-    // Configuration for production build
-    return {
-      plugins: [preact()],
-      base: '/Prosite/', 
-    };
-  }
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === 'production' || command === 'build';
+
+  return {
+    plugins: [preact()],
+    base: isProduction ? '/Prosite/' : '/',
+  };
 });
